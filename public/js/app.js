d9a77d0202146032f787a4cfe08153324995489e
Facundo Rubin <facundorubin@gmail.com>
2015-09-17 15:40:05 -0300

    Configure angular con los datos de url api/mesas

--- public/js/app.js
+++ public/js/app.js
@@ -1,70 +1,13 @@
 var app = angular.module('mesas', []);
 
 app.controller('MesasCtrl',['$scope','$http',function($scope,$http)
-{
+{	
+	$scope.tengoDatos=true;
 	 
-	$http.get("datos.json").success(function(data){
+	$http.get("api/mesas").success(function(data){
 		$scope.materias = data;
+		$scope.tengoDatos=false;
 		//console.log(data);
 	});
-
-	$scope.num2mes =function(num)
-	{
-
-		switch(num)
-		{
-			case("01"):
-				return "Enero";
-				break;
-
-			case("02"):
-				return "Febrero";
-				break;
-
-			case("03"):
-				return "Marzo";
-				break;
-
-			case("04"):
-				return "Abril";
-				break;	
-
-			case("05"):
-				return "Mayo";
-				break;	
-
-			case("06"):
-				return "Junio";
-				break;
-
-			case("07"):
-				return "Julio";
-				break;
-
-			case("08"):
-				return "Agosto";
-				break;
-
-			case("09"):
-				return "Septiembre";
-				break;
-
-			case("10"):
-				return "Octubre";
-				break;	
-
-			case("11"):
-				return "Noviembre";
-				break;
-
-			case("12"):
-				return "Diciembre";
-				break;			
-
-			default:
-				console.log("Error en num2mes("+num+")");
-        		return "Error"
-        		break;
-		}
-	}
-}]);
+}
+]);
