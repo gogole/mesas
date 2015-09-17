559b1670f3a5b9949db0568340208976ca1e55f0
Facundo Rubin <facundorubin@gmail.com>
2015-09-17 15:55:38 -0300

    Nueva filtro que ignora acentos

--- public/js/app.js
+++ public/js/app.js
@@ -9,5 +9,22 @@ app.controller('MesasCtrl',['$scope','$http',function($scope,$http)
 		$scope.tengoDatos=false;
 		//console.log(data);
 	});
+
+	function borrarAcentos(valor) {
+        return valor
+            .replace(/á/g, 'a')            
+            .replace(/é/g, 'e')
+            .replace(/í/g, 'i')
+            .replace(/ó/g, 'o')
+            .replace(/ú/g, 'u');
+    };
+
+    $scope.ignorarAcentos = function(item) {               
+        if (!$scope.filtro) return true;       
+
+        var text = borrarAcentos(item.nombre.toLowerCase())
+        var search = borrarAcentos($scope.filtro.toLowerCase());
+        return text.indexOf(search) > -1;
+    };
 }
 ]);
