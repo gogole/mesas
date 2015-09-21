29cd85d1c3273706823f8e763b33796815078eee
Facundo Rubin <facundorubin@gmail.com>
2015-09-21 18:12:06 -0300

    Agregue push state de html5

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 var app = angular.module('mesas', ['ngRoute']);
 
-app.config(function($routeProvider){
-  $routeProvider
+app.config(function($locationProvider,$routeProvider){
+	
+	$locationProvider.html5Mode({
+	  enabled: true,
+	  requireBase: false
+	});
+	
+	$routeProvider
 	  .when('/',{ 
 	     templateUrl:'parciales/main.html',
 	     controller: 'MesasCtrl'
